4bc2e68e9c9347cf8e34f67977855acd92fa121f
Shahll <130846011+Shahll@users.noreply.github.com>
2023-04-26 19:59:33 +0200

    Add files via upload
    я пытался взять юзер инпут и когда юезр высирает 0 то он сортирует список и даёт самое низкое и самое большое значение вот так вот

---
+++ task_one.py
@@ -0,0 +1,40 @@
+with open("task_one.txt", "w+") as file:
+    user_input = ''
+    while True:
+        x = input("Write an integer (enter 0 to stop): ")
+        if x == '0':
+            break
+        user_input += x + ','
+    file.write(user_input.strip())
+
+ 
+with open("task_one.txt", "r") as file:
+    user_input = [int(x) for x in file.read().split(',')]
+    user_input.sort()
+    print(user_input)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
