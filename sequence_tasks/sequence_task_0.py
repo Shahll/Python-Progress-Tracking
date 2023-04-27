cf80f0a2ebb057c33cb93bebe25751617a0b6682
Shahll <130846011+Shahll@users.noreply.github.com>
2023-04-27 20:23:20 +0200

    Update sequence_task_0.py

--- sequence_tasks/sequence_task_0.py
+++ sequence_tasks/sequence_task_0.py
@@ -1,9 +1,13 @@
 """
-Завдання на послідовність. 
  ! Послідовність чисел повинна закінчуватись 0. 
  ! Останнє число не повинно бути числом, яким закінчується послідовність. 
  ( тобто, у послідовності 1 2 3 4 0 останнє число, яке буде враховуватись для завдань - 4 ) 
  ! Ввід та вивід у консолі 
+
+ ! The sequence of numbers must end in 0. 
+ ! The last number must not be the number that ends the sequence. 
+ (i.e., in the sequence 1 2 3 4 0, the last number to be considered for the problems is 4) 
+ ! Input and output in the console
 """
 
 
