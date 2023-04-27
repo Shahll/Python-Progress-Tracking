308056a5ddc515f15093b05cb2d55942b07408a1
Shahll <130846011+Shahll@users.noreply.github.com>
2023-04-27 20:24:03 +0200

    Update sequence_task_2.py

--- sequence_tasks/sequence_task_2.py
+++ sequence_tasks/sequence_task_2.py
@@ -1,22 +1,13 @@
-""""
 
-! Завдання відсортовано за складністю
-1. Порахувати сумму чисел послідовності 
-2. Порахувати добуток чисел послідовності 
-3. Порахувати кількість чисел у послідовності 
-4. Мінімальне та максимальне числа послідовності
-5. Індекс (розташування, номер по порядку введення) мінімального та максимального числа послідовності 
-6. Кількість максимальних та мінімальних чисел у послідовності
+#2. Порахувати добуток чисел послідовності 
+#2. Calculate the product of the numbers in the sequence
 
-"""
-
-number = None
-sum = 1
+number = 1
+product = 1
 while number != 0:
-    number = int(input("Write an integer(enter 0 to stop): "))
-    if number != 0:
-        product *= number
-    else:
-        pass
-
+    product *= number
+    number = int(input("Write an integer(enter 0 to stop): ")) 
+    
 print(f"The product of all the numbers is {product}") 
+
+
