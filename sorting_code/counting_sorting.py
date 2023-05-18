bb7ac6eff610253ce59e6e0c015e9ccc7473aacf
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-18 19:58:45 +0200

    Add files via upload

---
+++ sorting_code/counting_sorting.py
@@ -0,0 +1,42 @@
+lst = [17, 0, 8, 20, 12, 13, 2, 4, 17, 18, 18, 16, 8, 3, 20, 2, 9, 17, 17, 12]
+counting_lst = [0]
+sorted_lst = []
+
+for _ in lst:
+    counting_lst.append(0)
+    sorted_lst.append(0)
+    
+def assign_numbers(list_to_assign, counting_list):
+    for assign in list_to_assign:
+        counting_list[assign] += 1
+    return counting_list
+
+
+def adding_numbers(list_to_add):
+    for add in range(1, len(list_to_add)):
+        list_to_add[add] += list_to_add[add - 1]
+    return list_to_add
+
+
+def counting_sort(list_to_sort, counting_list,sorted_list,):
+    """ 
+        Берем число с конца первого масива.. тоесть 12.. и смотрим на число с этим индексом во втором листе тобто 10..
+        и отнимаем единицу чтобы индексы не повторялись..
+        затем добавляем в третий лист на индекс 10 число 12 
+        
+    """
+    for assign in range(len(list_to_sort) - 1, -1, -1):
+        index_search = list_to_sort[assign]
+        temp = counting_list[index_search]
+        counting_list[index_search] -= 1
+        sorted_list[temp - 1] = list_to_sort[assign]
+    return sorted_list
+
+
+
+counting_lst = assign_numbers(lst,counting_lst)
+counting_lst = adding_numbers(counting_lst)
+sorted_lst = counting_sort(lst,counting_lst,sorted_lst)
+
+print(sorted_lst)
+
