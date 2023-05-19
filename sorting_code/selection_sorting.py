3da9db398500c52666f5191386dc7b4e30a72ed5
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-20 01:25:53 +0200

    Update selection_sorting.py

--- sorting_code/selection_sorting.py
+++ sorting_code/selection_sorting.py
@@ -1,11 +1,9 @@
-array = [1,3,6,-1,0,34]
-# -1 0 1 3 6 34
-
-
-for index, value in enumerate(array):
-    for count, item, in enumerate(array):
-        if value < item:
-            array[index],array[count] = array[count], array[index] 
-    
-        
-print(array)
+def selection_sort(array):
+    for index, value in enumerate(array):
+        for count, item, in enumerate(array):
+            if value < item:
+                array[index],array[count] = array[count], array[index] 
+    return array
+# Driver  
+lst = [1,3,6,-1,0,34]     
+print(selection_sort(lst))
