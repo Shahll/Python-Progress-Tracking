f7cb68fd3c8018dc0cb0ab8e6eecdaf3ab6a35e0
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-20 01:19:19 +0200

    Update bogo_sorting.py

--- sorting_code/bogo_sorting.py
+++ sorting_code/bogo_sorting.py
@@ -1,11 +1,10 @@
 import random
 
 def bogo_sort(array):
-    flag = True
     while array != sorted(array):
         a,b = random.randint(0,len(array) -1 ),random.randint(0,len(array)-1)
         array[a], array[b] = array[b], array[a]
     return array
 # Driver
 lst = [1, 2, 3, 1, 5, 6,4,42,]
-print(bogo_sort(array))
+print(bogo_sort(lst))
