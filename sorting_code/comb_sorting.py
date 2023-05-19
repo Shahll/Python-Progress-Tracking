39379ff176cb45d62e2bcb3fa8eeba67053800aa
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-20 01:22:04 +0200

    Update comb_sorting.py

--- sorting_code/comb_sorting.py
+++ sorting_code/comb_sorting.py
@@ -1,21 +1,17 @@
-array = [8,4,1,56,3]
-
-gap = len(array)
-
-sorted = False
-
-while sorted == False:
-    gap = int(gap // 1.3)
-    if gap <= 0:
-        sorted = True
-    
-    i = 0
-    
-    while i + gap != len(array) or i + gap < len(array):
-        if array[i] > array[i + gap]:
-            array[i + gap], array[i] = array[i] , array[i + gap]
-        i += 1
+def comb_sort(array):
+    gap = len(array)
+    sorted = False
+    while sorted == False:
+        gap = int(gap // 1.3)
+        if gap <= 0:
+            sorted = True
             
-            
-            
-print(array)
+        i = 0
+        while i + gap != len(array) or i + gap < len(array):
+            if array[i] > array[i + gap]:
+                array[i + gap], array[i] = array[i] , array[i + gap]
+            i += 1
+    return array
+# Driver       
+lst = [8,4,1,56,3]
+print(comb_sort(lst))
