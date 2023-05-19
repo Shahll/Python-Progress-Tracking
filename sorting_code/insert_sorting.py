80721c169d28a8b60586378fe5f90cd758ce5b33
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-20 01:23:31 +0200

    Update insert_sorting.py

--- sorting_code/insert_sorting.py
+++ sorting_code/insert_sorting.py
@@ -1,19 +1,13 @@
-array = [4,3,2,10,1,12,8]
 
-
-for i in range(1,len(array)): # takind index of a number strating at second number
-    
-    j = i # save an index in this value
-
-    while j > 0 and array[j - 1] > array[j]:   #  work when index not equals 0 or less and 
-                                               # when the number previously in the list will not be less than the next
-        
-        temp = array[j]  # temp value to save number we want ti insert
-        
-        array[j] = array[j-1]  # normal number is equals previous
-        array[j - 1] = temp  # previous number equals temp number
-
-        j = j -1 # decreasing an index
-        
-        
-print(array)
+def insertion_sort(array):
+    for i in range(1,len(array)): 
+        j = i 
+        while j > 0 and array[j - 1] > array[j]:  
+            temp = array[j]
+            array[j] = array[j-1]  
+            array[j - 1] = temp
+            j = j -1 
+    return array
+# Driver     
+lst  = [4,3,2,10,1,12,8]
+print(insertion_sort(lst))
