f2a92de409dac3d6a810b2199b7294cd352dacb4
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-21 23:12:05 +0200

    Update selection_sorting.py

--- sorting_code/selection_sorting.py
+++ sorting_code/selection_sorting.py
@@ -1,9 +1,14 @@
 def selection_sort(array):
-    for index, value in enumerate(array):
-        for count, item, in enumerate(array):
-            if value < item:
-                array[index],array[count] = array[count], array[index] 
+    n = len(array)
+    
+    for i in range(n):
+        min_index = i
+        for j in range(i, n ):
+            if array[j] < array[min_index]:
+                min_index = j
+        array[i], array[min_index]  = array[min_index] , array[i]
+        
     return array
 # Driver  
-lst = [1,3,6,-1,0,34]     
+lst = [1,3,6,-1,0,34,7,0,123,9]     
 print(selection_sort(lst))
