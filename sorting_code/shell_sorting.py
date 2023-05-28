4a1eb9e2fb2801b945ac0c2914bbc1881ead2369
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-28 10:49:37 +0200

    Update shell_sorting.py

--- sorting_code/shell_sorting.py
+++ sorting_code/shell_sorting.py
@@ -1,22 +1,21 @@
 def shell_sort(array):
     n = len(array)
     gap = n // 2
-
+    
     while gap > 0:
-
-        for i in range(gap, n):
+        
+        for i in range(gap,n):
             temp = array[i]
             j = i
-
+            
             while j >= gap and array[j - gap] > temp:
                 array[j] = array[j - gap]
                 j -= gap
-                array[j] = temp
-
-            gap = gap // 2
-
+                
+            array[j] = temp
+            
+        gap = gap // 2
     return array
-
 
 lst = [79, 28, 3, 7, 93, 55, 1, 0, 27, 69]
 print(shell_sort(lst))
