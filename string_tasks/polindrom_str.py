f9873854825ffdaddbc9d1b106cb91cfce6a3f9f
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-28 16:01:56 +0200

    Update polindrom_str.py

--- string_tasks/polindrom_str.py
+++ string_tasks/polindrom_str.py
@@ -3,6 +3,16 @@ def polindrom(str1):
         return True
     else:
         return False
+    
+def polindrom_loop(str1):
+    s = ""
+    for i in range(len(str1)-1, -1, -1):
+        s += str1[i]
+    if s == str1:
+        return True
+    else:
+        return False
+    
 # Driver
 str1 = "aboba"    
-print(polindrom(str1))
+print(polindrom_loop(str1))
