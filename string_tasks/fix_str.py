c7a9b08290f41c95bc994af499190e14478328ed
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-30 11:46:06 +0200

    Add files via upload

---
+++ string_tasks/fix_str.py
@@ -0,0 +1,42 @@
+
+""" - Переробити рядок знизу у рядок "алфАвІт, кОпчений, веснЯнИй, перенестИ"
+
+
+"""
+
+
+def fix_str(str1):
+    str1 = str1.split()
+    i = 0
+    while i != len(str1) - 1:
+        i += 1
+        if len(str1[i]) <= 3:
+            str1.pop(i)
+            i -= 1
+        elif str1[i].isnumeric() == True:
+            str1.pop(i)
+            i -= 1
+            
+    str1 = ', '.join(str1)
+    print(str1)
+    
+    
+    
+str1 = """ 
+алфАвІт 
+
+ц
+
+кОпчений 12
+
+т
+
+веснЯнИй  п
+
+в
+
+перенестИ в
+
+1
+"""
+fix_str(str1)
