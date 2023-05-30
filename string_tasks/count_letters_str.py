b2cd1d28fbda79264acbff37a1660c4e5716d379
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-30 20:30:26 +0200

    Update count_letters_str.py

--- string_tasks/count_letters_str.py
+++ string_tasks/count_letters_str.py
@@ -1,25 +1,20 @@
-from collections import Counter
-
 def counting_letters(str1):
     array_lower = [0] * 26 # отчет от 97 до 122  
     array_upper = [0] * 26 # отчёт от 65 до 90  
 
     for char in list(str1):
-        if char == char.upper():
-            array_upper[ord(char) - 65] += 1
         if char == char.lower():
             array_lower[ord(char) - 97] += 1
-    result = [f"{chr(index + 97)}" for index, value in enumerate(array_lower) for i in range(value)]
-    result = Counter(result)
+        if char == char.upper():
+            array_upper[ord(char) - 65] += 1
+    result = [chr(index + 97) for index, value in enumerate(array_lower) for i in range(value)]
     
-    for item, count in result.items():
-        print(f"{item}: {count}")
-        
+    print([(letter, result.count(letter)) for letter in set(result)])
+
     print("\n") 
-    result = [f"{chr(index + 65)}" for index, value in enumerate(array_upper) for i in range(value)]
-    result = Counter(result)
-    for item, count in result.items():
-        print(f"{item}: {count}")
+    result = [chr(index + 65) for index, value in enumerate(array_upper) for i in range(value)]
+    
+    print([(letter, result.count(letter)) for letter in set(result)])
     
 str1 = "abbcdejduiPONyanudBEBRAgaDADtfyoag"
 counting_letters(str1)
