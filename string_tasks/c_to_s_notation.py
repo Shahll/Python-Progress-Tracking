23dc23235f5a4d6042f0260a5694a88aed203b35
Shahll <130846011+Shahll@users.noreply.github.com>
2023-05-31 20:49:25 +0200

    Update c_to_s_notation.py

--- string_tasks/c_to_s_notation.py
+++ string_tasks/c_to_s_notation.py
@@ -3,8 +3,9 @@ def remake_text(str1):
     for index, char in enumerate(str1):
         if char.isupper():
             j += 1
-            str1 = str1.replace(char, char.lower())
+            str1 = str1.lower()
             str1 = str1[:index + j] + "_" + str1[index + j:]
+            
     print(str1)
 # Driver
 str1 = "someTextInSnakeNotation"
