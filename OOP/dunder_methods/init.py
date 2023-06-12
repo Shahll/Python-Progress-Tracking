3b7722b836741ccdbce7952b18822ba0cfa9f09c
Shahll <130846011+Shahll@users.noreply.github.com>
2023-06-13 01:09:07 +0200

    Create init.py

---
+++ OOP/dunder_methods/init.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        
+
+# Creating obkects, class Person
+
+person1 = Person("Shahll", 19)
+person2 = Person("Akane", 16)
+
+
