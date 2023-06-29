5d56f1ac69e4398b6d1da5eff46428a7fd26e65f
Shahll <130846011+Shahll@users.noreply.github.com>
2023-06-29 15:50:21 +0200

    Update quick_sorting.py

--- 3_sorting/quick_sorting.py
+++ 3_sorting/quick_sorting.py
@@ -1,18 +1,13 @@
-def quick_sort(array):
-    pivot = array[len(array) -1]
-    wall = 0
-    temp = None
-    while array != sorted(array):
-        for i in range(1,len(array)-1):
-            if array[i] < pivot:
-                array[wall], array[i] = array[i], array[wall]
-                wall += 1
-                print(f"wall is {wall}\narray is {array}\nindex is {i}\n")
-            if i == len(array)-1:
-                array.insert(wall,pivot)
-                pivot = array[len(array) -1]
-    return array
-    
-    
-lst = [12, 1, 77, 34, 8, 56, 6, 17, 28, 39]
-print(quick_sort(lst))
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[-1] 
+    smaller = [x for x in arr[:-1] if x <= pivot]  
+    greater = [x for x in arr[:-1] if x > pivot] 
+    return quicksort(smaller) + [pivot] + quicksort(greater)  
+
+
+arr = [4, 2, 8, 6, 1, 7, 3, 5]
+sorted_arr = quicksort(arr)
+print(sorted_arr)
+
