0a05e3bad336b670db283d7d842a8fdf942f4bb2
Shahll <130846011+Shahll@users.noreply.github.com>
2023-07-15 15:33:32 +0200

    Update quick_optimised_sorting.py

--- 3_sorting/quick_optimised_sorting.py
+++ 3_sorting/quick_optimised_sorting.py
@@ -12,22 +12,12 @@ def partition(array, low, high):
     return wall
 
 
-def median_of_three(array, low, high):
-    mid = (low + high) // 2
 
-    if array[mid] < array[low]:
-        array[mid], array[low] = array[low], array[mid]
-    if array[high] < array[low]:
-        array[high], array[low] = array[low], array[high]
-    if array[mid] > array[high]:
-        array[mid], array[high] = array[high], array[mid]
 
 
 def quick_sort(array, low, high):
     if low < high:
-
-        median_of_three(array, low, high)
-
+      
         wall = partition(array, low, high)
 
         quick_sort(array, low, wall - 1)
